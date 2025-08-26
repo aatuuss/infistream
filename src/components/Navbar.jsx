358885825b0834e194f65ac69d4d5f6d7db5fdfb
aatuuss <slnikmatus@gmail.com>
2025-08-26 10:06:11 +0700

    cover

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
             alt="Flowbite Logo"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Flowbitee
           </span>
         </a>
 
