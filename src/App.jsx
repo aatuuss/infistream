084e8f51ccb0a56aaaa374f0f34759aa0d72cf78
aatuuss <slnikmatus@gmail.com>
2025-09-03 10:59:32 +0700

    new page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
 import Footer from './components/Footer'
 import Home from './components/Home'
+import Simulasi from './app/Simulasi'
 import Navbar from './components/Navbar'
+import KonsultasiCFD from './app/KonsultasiCFD'
+import Layanan from './app/Layanan'
 import Page from './components/Page'
 import Portofolio from './components/Portofolio'
+import Portofolio1 from './app/Portofolio1'
 
 function App() {
   return (
     <>
     <Navbar/>
     <Home/>
+    <Simulasi />
+    <KonsultasiCFD/>
+    <Layanan/>
     {/* <Page/> */}
     <Portofolio/>
+    <Portofolio1/>
     <Footer/>
     </>
   )
